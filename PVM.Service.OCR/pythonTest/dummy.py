5c4d5d20312b2027244276ab4b67d1400567e8eb
GonzaloRomero <gromero@provimad.com>
2023-07-20 09:43:25 +0200

    Clase Dummy y requirements

---
+++ PVM.Service.OCR/pythonTest/dummy.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pytesseract
+
+class Dummy:
+
+    def __init__(*args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return self
+
+    def __getattr__(self, *args, **kwargs):
+        return self
+
+    def suma(x, y):
+        zeros = np.zeros(5)
+        suma = x + y
+        return suma
