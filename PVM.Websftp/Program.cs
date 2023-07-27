aea7c5e976785ece907febd543df7b0fc94ade4a
livingstoneCano23 <lcano@provimad.com>
2023-07-27 12:18:13 +0200

    Integracion proyecto SFTP, actualizacion

--- PVM.Websftp/Program.cs
+++ PVM.Websftp/Program.cs
@@ -8,21 +8,34 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Components.Web;
+using PVM.Websftp;
 
-namespace PVM.Websftp
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+//namespace PVM.Websftp
+//{
+//    public class Program
+//    {
+//        public static async Task Main(string[] args)
+//        {
+//            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//            builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddMudServices();
+//            builder.Services.AddMudServices();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+//            await builder.Build().RunAsync();
+//        }
+//    }
+//}
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7010/") });
+
+await builder.Build().RunAsync();
